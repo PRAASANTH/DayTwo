0e3c69334a43ed63777b7538377e024977b2b0a9
praasanth <praasanth@icloud.com>
2020-01-10 10:22:27 +0530

    added

---
+++ src/JavaDayTwo.java
@@ -0,0 +1,23 @@
+
+public class JavaDayTwo {
+
+		public void Studentname() {
+		System.out.println("Name:Vengat");
+		}
+		public void studentList() {
+		System.out.println();
+		}
+		public void StudentMark() {
+		System.out.println("Mark:1005");
+		}
+		public void StudentAddress() {
+		System.out.println("Address: Chennai");
+		}
+		public static void main(String[] arg) {
+		JavaDayTwo info = new JavaDayTwo();
+		info.Studentname();
+		info.StudentMark();
+		info.StudentAddress();
+		}
+		}
+
